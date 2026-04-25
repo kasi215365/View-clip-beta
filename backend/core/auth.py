8b2a9caa6bbac6ec57b28f83042baf5272f8da9b
kasi215365 <kaseemhepburn@gmail.com>
2026-04-24 21:33:45 -0400

    Update auth.py

--- backend/core/auth.py
+++ backend/core/auth.py
@@ -15,7 +15,7 @@ import os
 from datetime import datetime, timedelta, timezone
 
 import bcrypt
-import jwt
+jose import jwt
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
